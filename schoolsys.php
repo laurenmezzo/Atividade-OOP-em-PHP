c6e6004d56e8ef5e6e7f6b7c9c4f9aa10ff0b485
Lauren A.C. <mingufofo@gmail.com>
2026-09-15 09:14:05 -0300

    I'm so sleep y dsfiaaaa

---
+++ schoolsys.php
@@ -0,0 +1,21 @@
+<?php 
+class Person{
+    public $name;
+    public $age;
+    public $ssn;
+    public $email;
+    public $phonenum;
+    public $address;
+    public $city;
+    public $state;
+    public $birth;
+
+
+
+    public function registrate($centimetros){
+        
+    }
+    
+}
+
+?>
